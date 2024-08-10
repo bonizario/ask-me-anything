8c1e0ee6f0a591e33b743a2107d6a1afc0f48ff8
Gabriel Bonizário <56419828+bonizario@users.noreply.github.com>
2024-08-10 17:04:05 -0300

    feat: setup rest api endpoints

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/bonizario/ask-me-anything
 go 1.22.5
 
 require (
+	github.com/go-chi/chi v1.5.5
 	github.com/go-chi/chi/v5 v5.1.0
 	github.com/google/uuid v1.6.0
 	github.com/jackc/pgx/v5 v5.6.0
--- internal/api/api.go
+++ internal/api/api.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/bonizario/ask-me-anything/internal/store/pgstore"
+	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
 )
 
@@ -23,7 +24,44 @@ func NewHandler(q *pgstore.Queries) http.Handler {
 
 	r := chi.NewRouter()
 
+	r.Use(middleware.RequestID, middleware.Recoverer, middleware.Logger)
+
+	r.Route("/api", func(r chi.Router) {
+		r.Route("/rooms", func(r chi.Router) {
+			r.Post("/", a.handleCreateRoom)
+			r.Get("/", a.handleGetRooms)
+
+			r.Route("/{room_id}/messages", func(r chi.Router) {
+				r.Post("/", a.handleCreateRoomMessage)
+				r.Get("/", a.handleGetRoomMessages)
+
+				r.Route("/{message_id}", func(r chi.Router) {
+					r.Get("/", a.handleGetRoomMessage)
+					r.Patch("/react", a.handleReactToMessage)
+					r.Delete("/react", a.handleRemoveReactionFromMessage)
+					r.Patch("/answer", a.handleMarkMessageAsAnswered)
+				})
+			})
+		})
+	})
+
 	a.r = r
 
 	return a
 }
+
+func (h apiHandler) handleCreateRoom(w http.ResponseWriter, r *http.Request) {}
+
+func (h apiHandler) handleGetRooms(w http.ResponseWriter, r *http.Request) {}
+
+func (h apiHandler) handleCreateRoomMessage(w http.ResponseWriter, r *http.Request) {}
+
+func (h apiHandler) handleGetRoomMessages(w http.ResponseWriter, r *http.Request) {}
+
+func (h apiHandler) handleGetRoomMessage(w http.ResponseWriter, r *http.Request) {}
+
+func (h apiHandler) handleReactToMessage(w http.ResponseWriter, r *http.Request) {}
+
+func (h apiHandler) handleRemoveReactionFromMessage(w http.ResponseWriter, r *http.Request) {}
+
+func (h apiHandler) handleMarkMessageAsAnswered(w http.ResponseWriter, r *http.Request) {}
